6ac1c9789900f0b63478bd1ccc9456c562a45a40
MicheleCapezzani <michele.capezzani@gmail.com>
2019-06-06 16:06:55 +0200

    commit di prova dopo passaggio a sts

--- src/main/java/progettosilph/controller/ConfermaController.java
+++ src/main/java/progettosilph/controller/ConfermaController.java
@@ -19,6 +19,7 @@ public class ConfermaController extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 
+	// cazzucazzu
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
