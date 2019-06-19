70a0c3f113f47aedb9e59b83acfa6797b55297f5
MicheleCapezzani <michele.capezzani@gmail.com>
2019-06-19 15:27:57 +0200

    sistemazione css

--- src/main/java/progettosilph/repository/FunzionarioRepository.java
+++ src/main/java/progettosilph/repository/FunzionarioRepository.java
@@ -11,5 +11,4 @@ public interface FunzionarioRepository extends CrudRepository<Funzionario, Long>
 	//findByDatiDaRecuperare (sarebbero le variabili nel db)
 	public List<Funzionario> findByNome(String nome);
 	public List<Funzionario> findByNomeAndCognome(String nome, String cognome);
-	
 }
